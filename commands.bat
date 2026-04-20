8f52b4166f572844e49d64a840986fac6dfb7302
Nathaniel Ozuski <urbxnfits@gmail.com>
2026-04-20 11:41:26 +0100

    update batch file add pcgp

--- commands.bat
+++ commands.bat
@@ -1,15 +1,31 @@
-for /r "music-on-github" %f in (*.mp3) do (
-  ffmpeg -i "%f" -an -vcodec copy "..\ocarina\vids\%~nf.jpg"
+rem for /r "music-on-github" %f in (*.mp3) do (
+rem   ffmpeg -i "%f" -an -vcodec copy "..\ocarina\vids\%~nf.jpg"
+rem )
+
+rem for /r "music-on-github" %f in (*.mp3) do (
+rem   ffmpeg -loop 1 -i "vids\%~nf.jpg" -i "%f" -c:v libx264 -tune stillimage -c:a copy -shortest "..\ocarina\videos\%~nf.mp4"
+rem )
+
+rem for /r "music-on-github" %f in (*.mp3) do (
+rem   ffmpeg -loop 1 -i "vids\%~nf.jpg" -i "%f" -c:v libx264 -tune stillimage -c:a copy -shortest "..\ocarina\videos\%~nf.mkv"
+rem )
+
+rem for /r "music-on-github" %f in (*.mp3) do (
+rem   ffmpeg -loop 1 -i "vids\%~nf.jpg" -i "%f" -c:v libx264 -pix_fmt yuv420p -movflags +faststart -tune stillimage -c:a copy -shortest "..\ocarina\videos\%~nf.mp4"
+rem )
+
+rem for /r "..\music-on-github" %f in (*.mp3) do (
+rem   ffmpeg -loop 1 -i "vids\%~nf.jpg" -i "%f" -c:v libx264 -pix_fmt yuv420p -movflags +faststart -tune stillimage -c:a copy -shortest "..\ocarina\videos\%~nf.mp4"
+rem )
+
+
+
+rem in ocarina
+
+for /r "..\music-on-github" %f in (*.mp3) do (
+  ffmpeg -n -i "%f" -an -vcodec copy "vids\%~nf.jpg"
 )
 
-for /r "music-on-github" %f in (*.mp3) do (
-  ffmpeg -loop 1 -i "vids\%~nf.jpg" -i "%f" -c:v libx264 -tune stillimage -c:a copy -shortest "..\ocarina\videos\%~nf.mp4"
-)
-
-for /r "music-on-github" %f in (*.mp3) do (
-  ffmpeg -loop 1 -i "vids\%~nf.jpg" -i "%f" -c:v libx264 -tune stillimage -c:a copy -shortest "..\ocarina\videos\%~nf.mkv"
-)
-
-for /r "music-on-github" %f in (*.mp3) do (
-  ffmpeg -loop 1 -i "vids\%~nf.jpg" -i "%f" -c:v libx264 -pix_fmt yuv420p -movflags +faststart -tune stillimage -c:a copy -shortest "..\ocarina\videos\%~nf.mp4"
+for /r "..\music-on-github" %f in (*.mp3) do (
+  ffmpeg -n -loop 1 -i "vids\%~nf.jpg" -i "%f" -c:v libx264 -pix_fmt yuv420p -movflags +faststart -tune stillimage -c:a copy -shortest "videos\%~nf.mp4"
 )
