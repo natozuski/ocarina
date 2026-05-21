76c8965990de7c122eb6a92b5e80a73581f464ad
Nathaniel Ozuski <urbxnfits@gmail.com>
2026-05-21 14:41:33 +0100

    tidy commands add live cover

--- commands.bat
+++ commands.bat
@@ -8,9 +8,3 @@ for /r "..\music-on-github" %f in (*.mp3) do (
 for /r "..\music-on-github" %f in (*.mp3) do (
   ffmpeg -n -loop 1 -framerate 1 -i "pics\%~nf.jpg" -i "%f" -c:v libx264 -preset ultrafast -crf 30 -tune stillimage -pix_fmt yuv420p -movflags +faststart -c:a aac -b:a 192k -shortest "videos\%~nf.mp4"
 )
-
-rem youtube
-rem in /youtube
-rem converts videos to 720p
-
-for %f in (*.mp4) do ffmpeg -fflags +genpts -i "%f" -vf "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2,fps=30" -c:v libx264 -preset medium -crf 20 -c:a aac -b:a 128k "720p\%~nf_720p.mp4"
