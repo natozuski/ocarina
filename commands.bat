eb900e4e7be56c07379cfb34bc9b28340117656c
Nathaniel Ozuski <urbxnfits@gmail.com>
2026-05-24 15:25:38 +0100

    video fixer

--- commands.bat
+++ commands.bat
@@ -8,3 +8,26 @@ for /r "..\music-on-github" %f in (*.mp3) do (
 for /r "..\music-on-github" %f in (*.mp3) do (
   ffmpeg -n -loop 1 -framerate 1 -i "pics\%~nf.jpg" -i "%f" -c:v libx264 -preset ultrafast -crf 30 -tune stillimage -pix_fmt yuv420p -movflags +faststart -c:a aac -b:a 192k -shortest "videos\%~nf.mp4"
 )
+
+rem for editing videos to work everywhere
+
+ffmpeg -y ^
+-i "input.mp4" ^
+-c:v libx264 ^
+-profile:v high ^
+-level 4.1 ^
+-pix_fmt yuv420p ^
+-r 30 ^
+-g 60 ^
+-keyint_min 60 ^
+-sc_threshold 0 ^
+-crf 18 ^
+-preset medium ^
+-movflags +faststart ^
+-fflags +genpts ^
+-avoid_negative_ts make_zero ^
+-c:a aac ^
+-b:a 192k ^
+-ar 48000 ^
+-ac 2 ^
+"input_fixed.mp4"
