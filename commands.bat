13f2bc13dc024073c9a3332d56796daa0375f48c
Nathaniel <urbxnfits@gmail.com>
2026-06-01 19:52:33 +0100

    thumbnails II

--- commands.bat
+++ commands.bat
@@ -9,6 +9,15 @@ for /r "..\music-on-github" %f in (*.mp3) do (
   ffmpeg -n -loop 1 -framerate 1 -i "pics\%~nf.jpg" -i "%f" -c:v libx264 -preset ultrafast -crf 30 -tune stillimage -pix_fmt yuv420p -movflags +faststart -c:a aac -b:a 192k -shortest "videos\%~nf.mp4"
 )
 
+rem thumbnails
+
+ffmpeg -i input.mp4 -i thumbnail.png ^
+  -map 0 -map 1 ^
+  -c copy ^
+  -c:v:1 png ^
+  -disposition:v:1 attached_pic ^
+  output.mp4
+
 rem for editing videos to work everywhere
 
 ffmpeg -y ^
