57d78d2d2b97fd17e1d3afc75c0fbac93aaf6cc0
Nathaniel <urbxnfits@gmail.com>
2026-06-28 09:12:37 +0100

    alpha and primus

--- commands.bat
+++ commands.bat
@@ -1,11 +1,19 @@
 rem videos
 rem in (root)
 
-for /r "..\music-on-github" %f in (*.mp3) do (
+for /r "..\alpha_trion" %f in (*.mp3) do (
   ffmpeg -n -i "%f" -an -vcodec copy "pics\%~nf.jpg"
 )
 
-for /r "..\music-on-github" %f in (*.mp3) do (
+for /r "..\alpha_trion" %f in (*.mp3) do (
+  ffmpeg -n -loop 1 -framerate 1 -i "pics\%~nf.jpg" -i "%f" -c:v libx264 -preset ultrafast -crf 30 -tune stillimage -pix_fmt yuv420p -movflags +faststart -c:a aac -b:a 192k -shortest "videos\%~nf.mp4"
+)
+
+for /r "..\primus_prime" %f in (*.mp3) do (
+  ffmpeg -n -i "%f" -an -vcodec copy "pics\%~nf.jpg"
+)
+
+for /r "..\primus_prime" %f in (*.mp3) do (
   ffmpeg -n -loop 1 -framerate 1 -i "pics\%~nf.jpg" -i "%f" -c:v libx264 -preset ultrafast -crf 30 -tune stillimage -pix_fmt yuv420p -movflags +faststart -c:a aac -b:a 192k -shortest "videos\%~nf.mp4"
 )
 
