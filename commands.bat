146eedadfe50190b99fa9fc00e1490c01cb6f8f5
Nathaniel <urbxnfits@gmail.com>
2026-06-28 09:13:44 +0100

    typo

--- commands.bat
+++ commands.bat
@@ -9,11 +9,11 @@ for /r "..\alpha_trion" %f in (*.mp3) do (
   ffmpeg -n -loop 1 -framerate 1 -i "pics\%~nf.jpg" -i "%f" -c:v libx264 -preset ultrafast -crf 30 -tune stillimage -pix_fmt yuv420p -movflags +faststart -c:a aac -b:a 192k -shortest "videos\%~nf.mp4"
 )
 
-for /r "..\primus_prime" %f in (*.mp3) do (
+for /r "..\primon_prime" %f in (*.mp3) do (
   ffmpeg -n -i "%f" -an -vcodec copy "pics\%~nf.jpg"
 )
 
-for /r "..\primus_prime" %f in (*.mp3) do (
+for /r "..\primon_prime" %f in (*.mp3) do (
   ffmpeg -n -loop 1 -framerate 1 -i "pics\%~nf.jpg" -i "%f" -c:v libx264 -preset ultrafast -crf 30 -tune stillimage -pix_fmt yuv420p -movflags +faststart -c:a aac -b:a 192k -shortest "videos\%~nf.mp4"
 )
 
